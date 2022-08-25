1bdcebbe447d008b1975413e2cf7c2ebb5802521
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2022-08-26 01:40:24 +0300

    added infra

--- docker-monolith/infra/terraform/main.tf
+++ docker-monolith/infra/terraform/main.tf
@@ -25,13 +25,13 @@ resource "yandex_compute_instance" "app" {
   boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашем задании
-      image_id = "fd8fg4r8mrvoq6q2ve76"
+      image_id = ""
     }
   }
 
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9bem33uhju28r5i7pnu"
+    subnet_id = "enplkgfj4bjhhhd827dg"
     nat       = true
   }
 }
