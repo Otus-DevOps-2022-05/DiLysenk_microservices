db5608a22eaa0ec097971c9778bd57b13d4c056e
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2022-09-18 16:51:01 +0300

    page 6

---
+++ infra/terraform/main.tf
@@ -0,0 +1,51 @@
+# terraform init инициализация
+# terraform validate валидация
+# terraform fmt форматирование файлов к общему виду
+# terraform plan вывод изменений
+# terraform apply применяет изменения
+# terraform destroy
+
+data "yandex_compute_image" "ubuntu-20-04" {
+  family = "ubuntu-2004-lts"
+}
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  # имя создаваемой машины
+  name = "docker-vm"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      # указывается id образа  Yandex Cloud Marketplace
+      # https://cloud.yandex.ru/marketplace
+      image_id = data.yandex_compute_image.ubuntu-20-04.id
+      size = 60
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a (не vps, а подсеть vps)
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "service_account_key_file" {
+  description = "экспорт файла = key.json yc iam key create --service-account-id $ACCT_ID --output <свой путь>/key.json"
+}
+
+variable "cloud_id" {
+  description = "cloud_id индентификатор yc облака"
+}
+variable "folder_id" {
+  description = "folder_id каталог в yc"
+}
+variable "zone" {
+  description = "zone"
+  default     = "ru-central1-a"
+}
+
+
+variable "disk_id" {
+  description = "Disk image id default ubuntu 16 lts"
+  default     = "fd87k1od4v1bth3m59ha"
+}
+variable "subnet_id" {
+  description = "subnet for vm in yandex cloud"
+  default     = "enplkgfj4bjhhhd827dg"
+}
